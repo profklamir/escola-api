9b5a223623de8b8ccace94612d703222393a6c60
profklamir <profmirandia@gmail.com>
2025-03-26 12:01:38 -0300

    Configurando ROTAS

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+const app = express();
+const porta = 3000;
+
+/* CONFIGURANDO ROTAS */
+
+// raiz da API
+app.get('/', (req, res) => {
+    res.send(`API utilizando Node.js, Express e MySQL`);
+});
+
+// Exibindo TODOS os alunos
+app.get('/alunos', (req, res) => {
+    res.send(`Exibindo todos os alunos`);
+});
+
+// Exibindo UM aluno
+app.get('/alunos/1', (req, res) => {
+    res.send(`Exibindo dados de UM aluno`);
+});
+
+// INSERINDO/CADASTRANDO/ADICIONANDO um aluno
+app.post('/alunos', (req, res) => {
+    res.send(`Exibindo dados de UM aluno`);
+});
+
+
+// ATUALIZANDO aluno
+app.patch('alunos/1', (req, res) => {
+    res.send(`Atualizando dados do aluno`);
+});
+
+// EXCLUINDO aluno
+app.delete('alunos/1', (req, res) => {
+    res.send(`Aluno excluído com sucesso!`);
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
